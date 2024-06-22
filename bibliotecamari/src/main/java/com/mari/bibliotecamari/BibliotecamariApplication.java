cd87e269a80475d8a4c606f544a56f1751af54b6
Marineide Caires <marineidecaires@hotmail.com>
2024-06-22 09:56:07 -0300

    Primeiro commit

---
+++ bibliotecamari/src/main/java/com/mari/bibliotecamari/BibliotecamariApplication.java
@@ -0,0 +1,13 @@
+package com.mari.bibliotecamari;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecamariApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecamariApplication.class, args);
+	}
+
+}
---
+++ bibliotecamari/src/test/java/com/mari/bibliotecamari/BibliotecamariApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mari.bibliotecamari;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecamariApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
